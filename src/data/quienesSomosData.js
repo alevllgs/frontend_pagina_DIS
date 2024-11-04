cb331bb3a663a1ad4d1ba32778e7b1e529fec430
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-11-04 10:06:11 -0300

    sincroniza las img

--- src/data/quienesSomosData.js
+++ src/data/quienesSomosData.js
@@ -5,7 +5,7 @@ const quienesSomosData = [
         nombre: 'Kitana',
         cargo: 'Analista programador',
         titulo: 'Analista programador',
-        correo: 'kitana@uc.cl',
+        correo: 'kitana@gato.cl',
         fono: '+56 999999',
         imgSrc: 'public/img/quienes_somos/gato1.jpg',
 
@@ -15,7 +15,7 @@ const quienesSomosData = [
         nombre: 'Shao Kahn',
         cargo: 'Analista programador',
         titulo: 'Analista programador',
-        correo: 'shao_kahn@uc.cl',
+        correo: 'shao_kahn@gato.cl',
         fono: '+56 999999',
         imgSrc: 'public/img/quienes_somos/gato2.jpg',
 
@@ -26,9 +26,30 @@ const quienesSomosData = [
         nombre: 'Kitana',
         cargo: 'Analista programador',
         titulo: 'Analista programador',
-        correo: 'kitana@uc.cl',
+        correo: 'kitana@gato.cl',
         fono: '+56 999999',
         imgSrc: 'public/img/quienes_somos/gato3.jpg',
+
+    },
+
+    {
+        id: 4,
+        nombre: 'Kitana',
+        cargo: 'Analista programador',
+        titulo: 'Analista programador',
+        correo: 'kitana@gato.cl',
+        fono: '+56 999999',
+        imgSrc: 'public/img/quienes_somos/gato4.jpg',
+
+    },
+    {
+        id: 5,
+        nombre: 'Kitana',
+        cargo: 'Analista programador',
+        titulo: 'Analista programador',
+        correo: 'kitana@gato.cl',
+        fono: '+56 999999',
+        imgSrc: 'public/img/quienes_somos/gato4.jpg',
 
     },
     // Agrega más objetos según lo necesites
