4db4f409ebd116c0d4cb3e84e65223abc82f8f2e
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-11-14 11:14:25 -0300

    propuesta botones principales

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -11,18 +11,18 @@ const ButtonGroup = () => {
   return (
     <div className="buttons">
       <Button href="estadisticas/" icon="fa-chart-bar" title="Estadísticas REM" delay="0" iconColor={palette[0]} /> 
-      <Button href="egresos_hospitalarios/" icon="fa-hospital" title="Egresos Hospitalarios" delay="100" iconColor={palette[1]} /> 
-      <Button href="poblacion/" icon="fa-users" title="Población" delay="200" iconColor={palette[2]} /> 
       <Button href="lista_espera/" icon="fa-list-alt" title="Lista de Espera" delay="300" iconColor={palette[3]} /> 
-      <Button href="mortalidad_natalidad/" icon="fa-heartbeat" title="Mortalidad / Natalidad" delay="400" iconColor={palette[4]} />
-      <Button href="inmunizaciones/" icon="fa-syringe" title="R.N.I" delay="500" iconColor={palette[1]} /> 
-      <Button href="prest_centinelas/" icon="fa-user-md" title="Prestaciones" delay="600" iconColor={palette[2]} /> 
-      <Button href="https://app.powerbi.com/view?r=eyJrIjoiM2ZjNGI1ODQtODMwOS00NmI4LWE4ODAtMmZiMjk5NDNkZjI3IiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9" icon="fa-money-check-dollar" title="Gestión Clínica-Financiera" delay="700" iconColor={palette[3]} /> 
+      <Button href="ges/" icon="fa-hospital-user" title="GES" delay="100" iconColor={palette[1]} /> 
+      <Button href="grd/" icon="fa-bed-pulse" title="GRD" delay="200" iconColor={palette[2]} /> 
+      <Button href="indicadores/" icon="fa-chart-pie" title="Indicadores" delay="900" iconColor={palette[0]} /> 
+      
       <Button href="at_urgencia/" icon="fa-ambulance" title="Atenciones de Urgencia" delay="800" iconColor={palette[4]} /> 
-      <Button href="monitoreo_metas/" icon="fa-chart-pie" title="Monitoreo de Metas" delay="900" iconColor={palette[0]} /> 
-      <Button href="red_asistencial/" icon="fa-h-square" title="Red Asistencial" delay="1000" iconColor={palette[2]} /> 
-      <Button href="capsula/" icon="fa-graduation-cap" title="Capacitación" delay="1100" iconColor={palette[3]} /> 
-      <Button href="noticias/" icon="fa-newspaper" title="Noticias" delay="1200" iconColor={palette[4]} /> 
+      <Button href="inmunizaciones/" icon="fa-syringe" title="Inmunización" delay="500" iconColor={palette[1]} /> 
+      <Button href="mapa_derivacion/" icon="fa-person-walking-arrow-right" title="Mapa de derivación" delay="600" iconColor={palette[2]} /> 
+      
+      <Button href="https://app.powerbi.com/view?r=eyJrIjoiM2ZjNGI1ODQtODMwOS00NmI4LWE4ODAtMmZiMjk5NDNkZjI3IiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9" icon="fa-money-check-dollar" title="Gestión Clínica-Financiera" delay="700" iconColor={palette[3]} />
+      <Button href="visores_territoriales/" icon="fa-map-location-dot" title="Visores territoriales" delay="1200" iconColor={palette[4]} /> 
+      <Button href="poblacion/" icon="fa-people-group" title="Poblacion" delay="400" iconColor={palette[4]} />
     </div>
   );
 };
