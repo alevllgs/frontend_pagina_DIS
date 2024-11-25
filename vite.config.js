3f8e0742c010193b8bf9cfd69a92a3d68d0476dc
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-11-25 15:19:58 -0300

    sirve para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Detectar si estás en desarrollo o en producción
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: isProduction ? '/segun_ejemplo/' : '/', // Cambia 'segun_ejemplo' por el nombre de tu repositorio
+});
