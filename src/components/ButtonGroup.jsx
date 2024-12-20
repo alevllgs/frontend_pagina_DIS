321c6e2877b0a3ae30eda12167d8bf6fd6c8a4bc
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-12-20 16:21:55 -0300

    agrega nueva pagina gestion clinica financiera

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -22,7 +22,7 @@ const ButtonGroup = () => {
       <Button href="inmunizaciones/" icon="fa-syringe" title="Inmunización" delay="500" iconColor={palette[1]} /> 
       <Button href="https://app.powerbi.com/view?r=eyJrIjoiOWJmNThmNDYtMmU5Mi00MzNiLWJmMzctNmEyZjA2NGY5NWRjIiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9" icon="fa-person-walking-arrow-right" title="Mapa de derivación" delay="600" iconColor={palette[2]} /> 
       
-      <Button href="https://app.powerbi.com/view?r=eyJrIjoiM2ZjNGI1ODQtODMwOS00NmI4LWE4ODAtMmZiMjk5NDNkZjI3IiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9" icon="fa-money-check-dollar" title="Gestión Clínica-Financiera" delay="700" iconColor={palette[3]} />
+      <Button href="gestion_clinica_financiera/" icon="fa-money-check-dollar" title="Gestión Clínica-Financiera" delay="700" iconColor={palette[3]} />
       <Button href="visores_territoriales/" icon="fa-map-location-dot" title="Visores territoriales" delay="1200" iconColor={palette[4]} /> 
       <Button href="poblacion/" icon="fa-people-group" title="Población" delay="400" iconColor={palette[4]} />
     </div>
--- src/data/globalIndex.js
+++ src/data/globalIndex.js
@@ -22,7 +22,7 @@ const globalIndex = [
         },
         {
           title: "Gestión Clínica-Financiera",
-          link: "https://app.powerbi.com/view?r=eyJrIjoiM2ZjNGI1ODQtODMwOS00NmI4LWE4ODAtMmZiMjk5NDNkZjI3IiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9",
+          link: "gestion_clinica_financiera/",
         },
         { title: "Visores territoriales", link: "/visores_territoriales/" },
         { title: "Población", link: "/poblacion/" },
@@ -31,6 +31,49 @@ const globalIndex = [
     {
       page: "GES",
       path: "/ges",
+      content: [
+        {
+          title: "Garantías Retrasadas GES (bimensual)",
+          link: "https://app.powerbi.com/view?r=eyJrIjoiYWM0OGMxNWItYmMyMC00ZDM5LWEwZWMtNzEwOTBhODQwMjQ2IiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9",
+        },
+        {
+          title: "Cumplimiento de Garantías GES (mensual)",
+          link: "https://app.powerbi.com/view?r=eyJrIjoiYTE5Y2RjNmYtZmFlMy00NDc1LThhYzUtNWM3Zjc1ODE2OTY2IiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9",
+        },
+        {
+          title: "Garantías Exceptuadas Transitorias",
+          link: "https://app.powerbi.com/view?r=eyJrIjoiNjIzNGI0ODAtY2FhOC00YWVkLThhMTgtMmFkZTFhMDRiYmI3IiwidCI6IjA2MDc0N2E5LTk5YjYtNDg4NS1hNmQxLWI3ZDg1OGE4M2E1MyJ9",
+        },
+        { title: "Mapa derivación GES (flujo)", link: "/pagina_sigges/protocolos_ges.html" },
+        {
+          title: "Circular 195 Super Salud",
+          link: "https://dis.saludoriente.cl/degidssmo/ges/Circular-195%20(DAU%20Notificaci%C3%B3n)%202013.pdf",
+        },
+        {
+          title: "Circular 227 Super Salud",
+          link: "https://dis.saludoriente.cl/degidssmo/ges/CircularN%C2%BA227_Modifica%20Constancia%20GES.pdf",
+        },
+        {
+          title: "Circular 288 Super Salud",
+          link: "https://dis.saludoriente.cl/degidssmo/ges/Cirular%20SIS%20288%20Modificaciones%20Excepciones%20GO_%2006%2007%202017.pdf",
+        },
+        {
+          title: "Ordinario MINSAL N° 2134",
+          link: "https://dis.saludoriente.cl/degidssmo/ges/Ord%202134%20nueva%20intrucciones%20GES%20mayo%202018.pdf",
+        },
+        {
+          title: "Nueva Nomenclatura Garantias",
+          link: "https://dis.saludoriente.cl/degidssmo/ges/Nueva%20Nomenclatura%20de%20Garant%C3%ADas%20v1%20%202018.pdf",
+        },
+        {
+          title: "Diario Oficial Decreto GES 2019",
+          link: "https://dis.saludoriente.cl/degidssmo/ges/Publicacion%20diario%20oficial.%20Nuevo%20Decreto%20GES%202019.pdf",
+        },
+      ],
+    },
+    {
+      page: "Gestión Clinica Financiera",
+      path: "/gestion_clinica_financiera",
       content: [
         {
           title: "Garantías Retrasadas GES (bimensual)",
