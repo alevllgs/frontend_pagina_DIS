2af17dbd7a558b6d32950c70c754396b105570ac
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2025-01-09 17:09:13 -0300

    agrega estilo a los input de subir rem

--- src/pages/RemProtegido.jsx
+++ src/pages/RemProtegido.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import Footer from "../components/Footer"; // Importar el Footer
+import "../styles/RemProtegido.css"; // Importar los estilos específicos para este componente
 
 const RemProtegido = () => {
   return (
-    <div>
-      <h1>Subir Archivos REM</h1>
-      <form>
-        <input type="file" accept=".xlsx,.xlsm" />
-        <input type="file" accept=".xlsx,.xlsm" />
-        <input type="file" accept=".xlsx,.xlsm" />
-        <input type="file" accept=".xlsx,.xlsm" />
-        <button type="submit">Subir Archivos</button>
-      </form>
+    <div className="form-page">
+      <div className="form-container">
+        <h1>Subir Archivos REM</h1>
+        <form>
+          <input type="file" accept=".xlsx,.xlsm" />
+          <input type="file" accept=".xlsx,.xlsm" />
+          <input type="file" accept=".xlsx,.xlsm" />
+          <input type="file" accept=".xlsx,.xlsm" />
+          <button type="submit">Subir Archivos</button>
+        </form>
+      </div>
+      {/* Footer al final de la página */}
+      <Footer />
     </div>
   );
 };
 
 export default RemProtegido;
+
