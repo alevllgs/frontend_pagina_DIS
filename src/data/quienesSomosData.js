285ae9afcd48cc72556338439f97853d19a0a16a
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2025-02-12 10:20:48 -0300

    actualiza estilos de la tarjeta en QuienesSomos.css, corrige datos en problemas_salud.csv y agrega nuevos miembros al equipo en quienesSomosData.js

--- src/data/quienesSomosData.js
+++ src/data/quienesSomosData.js
@@ -2,51 +2,93 @@
 const quienesSomosData = [
     {
         id: 1,
-        nombre: 'Kitana',
-        cargo: 'Analista programador',
-        titulo: 'Analista programador',
+        nombre: 'Marcela Zolezzi',
+        cargo: 'Jefe Departamento',
+        titulo: 'Matrona, Magister en Salud Pública',
         correo: 'kitana@gato.cl',
         fono: '+56 999999',
-        imgSrc: 'public/img/quienes_somos/gato1.jpg',
+        imgSrc: 'public/img/quienes_somos/MARCELA.jpg',
 
     },
     {
         id: 2,
-        nombre: 'Shao Kahn',
-        cargo: 'Analista programador',
-        titulo: 'Analista programador',
+        nombre: 'Patricio Medina',
+        cargo: 'Jefe Unidad Análisis de Información en Salud',
+        titulo: 'Informático Biomédico e Ingeniero en Computación e Informática',
         correo: 'shao_kahn@gato.cl',
         fono: '+56 999999',
-        imgSrc: 'public/img/quienes_somos/gato2.jpg',
+        imgSrc: 'public/img/quienes_somos/PATRICIOM.jpg',
 
     },
 
     {
         id: 3,
-        nombre: 'Kitana',
-        cargo: 'Analista programador',
-        titulo: 'Analista programador',
+        nombre: 'Angelica Sepulveda',
+        cargo: 'Jefe Unidad de Estadística y Gestión Administrativa de Pacientes',
+        titulo: 'Ingeniero de Ejecución en Gestión Pública',
         correo: 'kitana@gato.cl',
         fono: '+56 999999',
-        imgSrc: 'public/img/quienes_somos/gato3.jpg',
+        imgSrc: 'public/img/quienes_somos/ANGELICA.jpg',
 
     },
 
     {
         id: 4,
-        nombre: 'Kitana',
-        cargo: 'Analista programador',
-        titulo: 'Analista programador',
+        nombre: 'Marcos Antihuala',
+        cargo: 'Unidad análisis de información en Salud',
+        titulo: 'Ingeniero Industrial',
         correo: 'kitana@gato.cl',
         fono: '+56 999999',
-        imgSrc: 'public/img/quienes_somos/gato4.jpg',
+        imgSrc: 'public/img/quienes_somos/MARCOS.jpg',
 
     },
     {
         id: 5,
-        nombre: 'Kitana',
-        cargo: 'Analista programador',
-        titulo: 'Analista programador',
+        nombre: 'Alejandro Villegas',
+        cargo: 'Unidad análisis de información en Salud',
+        titulo: 'Ingeniero Comercial, Programador',
+        correo: 'kitana@gato.cl',
+        fono: '+56 999999',
+        imgSrc: 'public/img/quienes_somos/ALEJANDRO.jpg',
+
+    },
+
+    {
+        id: 6,
+        nombre: 'Patricio Acevedo',
+        cargo: 'Unidad de Estadística y Gestión Administrativa de Pacientes',
+        titulo: 'Ingeniero Comercial',
+        correo: 'kitana@gato.cl',
+        fono: '+56 999999',
+        imgSrc: 'public/img/quienes_somos/gato3.jpg',
+
+    },
+    {
+        id: 7,
+        nombre: 'Héctor Gutiérrez',
+        cargo: 'Unidad análisis de información en Salud',
+        titulo: 'Ingeniero Estadístico',
+        correo: 'kitana@gato.cl',
+        fono: '+56 999999',
+        imgSrc: 'public/img/quienes_somos/HECTOR.jpeg',
+
+    },
+
+    {
+        id: 8,
+        nombre: 'Carolina Bravo',
+        cargo: 'Departamento de Información Sanitaria',
+        titulo: 'Secretaría y soporte administrativo',
+        correo: 'kitana@gato.cl',
+        fono: '+56 999999',
+        imgSrc: 'public/img/quienes_somos/CAROLINA.jpg',
+
+    },
+    {
+        id: 8,
+        nombre: 'Carlos Vielma',
+        cargo: 'Departamento de Información Sanitaria',
+        titulo: 'Secretaría y soporte administrativo',
         correo: 'kitana@gato.cl',
         fono: '+56 999999',
         imgSrc: 'public/img/quienes_somos/gato4.jpg',
