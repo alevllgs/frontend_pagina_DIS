0993b0ea3fb7bf3237030eebf898ddd8fd21bf2a
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2025-02-12 15:26:15 -0300

    agrega funcionalidad de carrusel y control de visibilidad en CreaBanner.jsx, mejora estilos en CreaBanner.css y ajusta estilos en Navbar.css y QuienesSomos.css

--- src/components/CreaBanner.jsx
+++ src/components/CreaBanner.jsx
@@ -1,37 +1,47 @@
 import React, { useState, useEffect } from "react";
-import "../styles/CreaBanner.css"; // Asegúrate de tener este archivo CSS para los estilos
+import "../styles/CreaBanner.css";
 
 const banners = [
-  "public/img/banner/banner1.jpg",
-  // Agrega más rutas de imágenes si es necesario
+  "/img/banner/banner1.jpg",
+  // Agrega más imágenes si necesitas
 ];
 
 const CreaBanner = () => {
   const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
 
+  // Carrusel de imágenes
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentBannerIndex((prevIndex) => (prevIndex + 1) % banners.length);
-    }, 3000); // Cambia de imagen cada 3 segundos
+      setCurrentBannerIndex((prev) => (prev + 1) % banners.length);
+    }, 3000);
     return () => clearInterval(interval);
   }, []);
 
+  // Control del scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setIsVisible(scrollTop < 50); // 50px de margen para evitar rebote
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="banner-container">
+    <div className={`banner-container ${!isVisible ? "hidden" : ""}`}>
       <img
         src={banners[currentBannerIndex]}
         alt="Banner"
         className="banner-image"
       />
-      {/* Texto superpuesto */}
       <div className="banner-text">
-       
-        <p>
-          Bienvenido al sitio web del Departamento de Información Sanitaria (DIS) del Servicio de
-          Salud Metropolitano Oriente (SSMO). Aquí podrá encontrar información relevante generada por
-          los distintos establecimientos pertenecientes al SSMO, la cual se irá actualizando
-          periódicamente.
-        </p>
+        <p>Bienvenido al sitio web del Departamento de Información Sanitaria
+          (DIS) del Servicio de Salud Metropolitano Oriente (SSMO). Aquí podrá
+          encontrar información relevante generada por los distintos
+          establecimientos pertenecientes al SSMO, la cual se irá actualizando
+          periódicamente.</p>
       </div>
     </div>
   );
