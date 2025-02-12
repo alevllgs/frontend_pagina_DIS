fc97e7d2738ca04465a610ea9a06f9dc0778461d
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2025-02-12 11:25:31 -0300

    elimina información de contacto de miembros en quienesSomosData.js y mejora estilos de tarjetas en QuienesSomos.css para mayor responsividad y apariencia

--- src/data/quienesSomosData.js
+++ src/data/quienesSomosData.js
@@ -5,8 +5,7 @@ const quienesSomosData = [
         nombre: 'Marcela Zolezzi',
         cargo: 'Jefe Departamento',
         titulo: 'Matrona, Magister en Salud Pública',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/MARCELA.jpg',
 
     },
@@ -15,8 +14,7 @@ const quienesSomosData = [
         nombre: 'Patricio Medina',
         cargo: 'Jefe Unidad Análisis de Información en Salud',
         titulo: 'Informático Biomédico e Ingeniero en Computación e Informática',
-        correo: 'shao_kahn@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/PATRICIOM.jpg',
 
     },
@@ -26,8 +24,7 @@ const quienesSomosData = [
         nombre: 'Angelica Sepulveda',
         cargo: 'Jefe Unidad de Estadística y Gestión Administrativa de Pacientes',
         titulo: 'Ingeniero de Ejecución en Gestión Pública',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/ANGELICA.jpg',
 
     },
@@ -37,8 +34,7 @@ const quienesSomosData = [
         nombre: 'Marcos Antihuala',
         cargo: 'Unidad análisis de información en Salud',
         titulo: 'Ingeniero Industrial',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/MARCOS.jpg',
 
     },
@@ -47,8 +43,7 @@ const quienesSomosData = [
         nombre: 'Alejandro Villegas',
         cargo: 'Unidad análisis de información en Salud',
         titulo: 'Ingeniero Comercial, Programador',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/ALEJANDRO.jpg',
 
     },
@@ -58,8 +53,7 @@ const quienesSomosData = [
         nombre: 'Patricio Acevedo',
         cargo: 'Unidad de Estadística y Gestión Administrativa de Pacientes',
         titulo: 'Ingeniero Comercial',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/gato3.jpg',
 
     },
@@ -68,8 +62,7 @@ const quienesSomosData = [
         nombre: 'Héctor Gutiérrez',
         cargo: 'Unidad análisis de información en Salud',
         titulo: 'Ingeniero Estadístico',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/HECTOR.jpeg',
 
     },
@@ -79,8 +72,7 @@ const quienesSomosData = [
         nombre: 'Carolina Bravo',
         cargo: 'Departamento de Información Sanitaria',
         titulo: 'Secretaría y soporte administrativo',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+    
         imgSrc: 'public/img/quienes_somos/CAROLINA.jpg',
 
     },
@@ -89,8 +81,7 @@ const quienesSomosData = [
         nombre: 'Carlos Vielma',
         cargo: 'Departamento de Información Sanitaria',
         titulo: 'Secretaría y soporte administrativo',
-        correo: 'kitana@gato.cl',
-        fono: '+56 999999',
+
         imgSrc: 'public/img/quienes_somos/gato4.jpg',
 
     },
